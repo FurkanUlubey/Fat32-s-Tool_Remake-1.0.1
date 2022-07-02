e7edcd559be360d25694ab366f914cc19934c002
Ulubey <99231918+FurkanUlubey@users.noreply.github.com>
2022-07-02 05:59:51 +0300

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Remake
+func main() {
+	fmt.Println("Fat32's Tool Remake")
+	fmt.Println("1.0.1 Release")
+	fmt.Println("2.07.2022 05:34")
+	fmt.Println("Web --> http://www.xenteam.ml/?i=1 ")
+	fmt.Println("By: Ulubey")
+}
